98267211ee5e6b552d6087a35b4be9ec59dbc8b3
Maverick <maverick.schirmer@gmail.com>
2019-11-11 20:10:44 -0800

    Ended db

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -38,42 +38,113 @@ if(err){
         switch(item_number){
             case '1':
                 console.log(`You have chosen to buy an order of ${quantity} ${data[0].product_name}`)
+
                 if(quantity <= data[0].stock_quantity){
                     console.log(`Congratulations your order has been placed`)
-                    db.query(`INSERT INTO products(stock_quantity) VALUES (${data[0].stock_quantity - quantity}) `)
+                    let remaining_inventory = data[0].stock_quantity - quantity
+                    db.query(`UPDATE products SET stock_quantity = ${remaining_inventory} WHERE item_id = 1 `)
+                    
                 }else{console.log(`Sorry we have insufficient quantity of that item we only have ${data[0].stock_quantity}`)}
+                db.end()
                 break
+
             case '2':
                     console.log(`You have chosen to buy an order of ${quantity} ${data[1].product_name}s`)
+
+                    if(quantity <= data[1].stock_quantity){
+                        console.log(`Congratulations your order has been placed`)
+                        let remaining_inventory = data[1].stock_quantity - quantity
+                        db.query(`UPDATE products SET stock_quantity = ${remaining_inventory} WHERE item_id = 2 `)
+                        
+                    }else{console.log(`Sorry we have insufficient quantity of that item we only have ${data[1].stock_quantity}`)}
+                    db.end()
                 break
+
             case '3':
                     console.log(`You have chosen to buy an order of ${quantity} ${data[2].product_name}s`)
+
+                    if(quantity <= data[2].stock_quantity){
+                        console.log(`Congratulations your order has been placed`)
+                        let remaining_inventory = data[2].stock_quantity - quantity
+                        db.query(`UPDATE products SET stock_quantity = ${remaining_inventory} WHERE item_id = 3 `)
+                        
+                    }else{console.log(`Sorry we have insufficient quantity of that item we only have ${data[2].stock_quantity}`)}
+                    db.end()
                 break
             case '4':
                     console.log(`You have chosen to buy an order of ${quantity} ${data[3].product_name}`)
+                    if(quantity <= data[3].stock_quantity){
+                        console.log(`Congratulations your order has been placed`)
+                        let remaining_inventory = data[3].stock_quantity - quantity
+                        db.query(`UPDATE products SET stock_quantity = ${remaining_inventory} WHERE item_id = 4 `)
+                        
+                    }else{console.log(`Sorry we have insufficient quantity of that item we only have ${data[3].stock_quantity}`)}
+                    db.end()
                 break
             case '5':
                     console.log(`You have chosen to buy an order of ${quantity} ${data[4].product_name}s`)
+                    if(quantity <= data[4].stock_quantity){
+                        console.log(`Congratulations your order has been placed`)
+                        let remaining_inventory = data[4].stock_quantity - quantity
+                        db.query(`UPDATE products SET stock_quantity = ${remaining_inventory} WHERE item_id = 5 `)
+                        
+                    }else{console.log(`Sorry we have insufficient quantity of that item we only have ${data[4].stock_quantity}`)}
+                    db.end()
                 break
             case '6':
                     console.log(`You have chosen to buy an order of ${quantity} ${data[5].product_name}`)
+                    if(quantity <= data[5].stock_quantity){
+                        console.log(`Congratulations your order has been placed`)
+                        let remaining_inventory = data[5].stock_quantity - quantity
+                        db.query(`UPDATE products SET stock_quantity = ${remaining_inventory} WHERE item_id = 6 `)
+                        
+                    }else{console.log(`Sorry we have insufficient quantity of that item we only have ${data[5].stock_quantity}`)}
+                    db.end()
                 break
             case '7':
                     console.log(`You have chosen to buy an order of ${quantity} ${data[6].product_name}`)
+                    if(quantity <= data[6].stock_quantity){
+                        console.log(`Congratulations your order has been placed`)
+                        let remaining_inventory = data[6].stock_quantity - quantity
+                        db.query(`UPDATE products SET stock_quantity = ${remaining_inventory} WHERE item_id = 7 `)
+                        
+                    }else{console.log(`Sorry we have insufficient quantity of that item we only have ${data[6].stock_quantity}`)}
+                    db.end()
                 break
             case '8':
                     console.log(`You have chosen to buy an order of ${quantity} ${data[7].product_name}s`)
+                    if(quantity <= data[7].stock_quantity){
+                        console.log(`Congratulations your order has been placed`)
+                        let remaining_inventory = data[7].stock_quantity - quantity
+                        db.query(`UPDATE products SET stock_quantity = ${remaining_inventory} WHERE item_id = 8 `)
+                        
+                    }else{console.log(`Sorry we have insufficient quantity of that item we only have ${data[7].stock_quantity}`)}
+                    db.end()
                 break
             case '9':
                     console.log(`You have chosen to buy an order of ${quantity} ${data[8].product_name}s`)
+                    if(quantity <= data[8].stock_quantity){
+                        console.log(`Congratulations your order has been placed`)
+                        let remaining_inventory = data[8].stock_quantity - quantity
+                        db.query(`UPDATE products SET stock_quantity = ${remaining_inventory} WHERE item_id = 9 `)
+                        
+                    }else{console.log(`Sorry we have insufficient quantity of that item we only have ${data[8].stock_quantity}`)}
+                    db.end()
                 break
             case '1o':
                     console.log(`You have chosen to buy an order of ${quantity} ${data[9].product_name}`)
+                    if(quantity <= data[9].stock_quantity){
+                        console.log(`Congratulations your order has been placed`)
+                        let remaining_inventory = data[9].stock_quantity - quantity
+                        db.query(`UPDATE products SET stock_quantity = ${remaining_inventory} WHERE item_id = 10 `)
+                        
+                    }else{console.log(`Sorry we have insufficient quantity of that item we only have ${data[9].stock_quantity}`)
+                    db.end()
+                }
+                    
                 break    
                 
         }
     })
     
-    // console.log(data[0].item_id)
-
 })
